83994e5d15ee15e9c47ced09c5afaa0b930b4341
aaamam <aaamam@c4r2p4.1337.ma>
2024-01-10 18:48:59 +0100

    update

---
+++ src/client.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaamam <aaamam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 13:20:54 by aaamam            #+#    #+#             */
+/*   Updated: 2024/01/10 18:27:29 by aaamam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minitalk.h"
+
+static int	ft_atoi(const char *str)
+{
+	int					i;
+	int					sign;
+	unsigned long int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+void	ft_atob(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+        if (c & (0x01 << bit))
+        {
+            if (kill(pid, SIGUSR1) == -1)
+            {
+                ft_printf("%s\n", C_ERROR);
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            if (kill(pid, SIGUSR2) == -1)
+            {
+                ft_printf("%s\n", C_ERROR);
+                exit(EXIT_FAILURE);
+            }
+        }
+		usleep(500);
+		bit++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		while (argv[2][i] != '\0')
+		{
+			ft_atob(pid, argv[2][i]);
+			i++;
+		}
+	}
+	else
+	{
+		ft_printf("%s\n", C_ERROR);
+        ft_printf("%s\n", C_ARGS);
+        ft_printf("%s\n", C_EX_ARGS);
+		return (1);
+	}
+	return (0);
+}
---
+++ src/server.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaamam <aaamam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 16:59:33 by aaamam            #+#    #+#             */
+/*   Updated: 2024/01/10 18:29:09 by aaamam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minitalk.h"
+
+void	ft_btoa(int sig)
+{
+	static int	bit;
+	static int	i;
+
+	if (sig == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+    int	pid;
+
+    (void)argv;
+    if (argc != 1)
+    {
+        ft_printf("%s\n", C_ERROR);
+        return (1);
+    }
+    pid = getpid();
+    ft_printf(S_PID, pid);
+    while (argc == 1)
+    {
+        if (signal(SIGUSR1, ft_btoa) == SIG_ERR)
+        {
+            ft_printf("%s\n", SA_USR1_FAIL);
+            exit(EXIT_FAILURE);
+        }
+        if (signal(SIGUSR2, ft_btoa) == SIG_ERR)
+        {
+            ft_printf("%s\n", SA_USR2_FAIL);
+            exit(EXIT_FAILURE);
+        }
+        pause ();
+    }
+    return (0);
+}
